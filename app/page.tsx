6b0b535ab3711bcebca6f8cc670c893184b9ffc3
André Suárez Schou <98514958+suarezschou@users.noreply.github.com>
2025-07-20 10:53:16 +0200

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,25 @@ import Link from 'next/link'
 export default function Home() {
   return (
     // Wrap everything in a single parent div
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 gap-30 p-30">
-      <p className="text-4xl font-bold text-blue-600 tracking-wide">
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100 gap-10 p-10">
+      <p className="text-2xl font-bold text-blue-600 tracking-wide p-10">
         TheRushMaster: Age of Empires 2 player and streamer
       </p>
       <Link href="https://www.kick.com/therushmaster">
         <Image
           src="/kick.svg"
           alt="logo of twitch"
-          width={500}
-          height={500}
-          className="animate-bounce"
+          width={300}
+          height={200}
+          className="animate-bounce mb-10"
           />
        </Link>
       <Link href="https://www.twitch.tv/therushmaster"> 
       <Image
         src="/twitch.svg"
         alt="logo of twitch"
-        width={500}
-        height={500}
+        width={150}
+        height={100}
         className="animate-pulse"
       />
       </Link>
@@ -31,8 +31,8 @@ export default function Home() {
       <Image
         src="/youtube.svg"
         alt="logo of twitch"
-        width={500}
-        height={500}
+        width={200}
+        height={200}
         className="animate-pulse"
       />
       </Link>
